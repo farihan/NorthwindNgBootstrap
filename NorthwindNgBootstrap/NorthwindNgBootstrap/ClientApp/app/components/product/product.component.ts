05117d05946aaed7f51ece31a383d14b6b2def62
Muhamad Farihan <farihan@gmail.com>
2017-09-16 23:01:47 +0800

    add product details

--- NorthwindNgBootstrap/NorthwindNgBootstrap/ClientApp/app/components/product/product.component.ts
+++ NorthwindNgBootstrap/NorthwindNgBootstrap/ClientApp/app/components/product/product.component.ts
@@ -1,17 +1,26 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/Observable/forkJoin';
+
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 import { NotificationService } from './../../services/notification.service';
 import { Product } from './../../models/product';
 import { ProductService } from './../../services/product.service';
+import { CategoryService } from './../../services/category.service';
+import { SupplierService } from './../../services/supplier.service';
 
 @Component({
     selector: 'productlist',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css']
+
 })
 
 export class ProductComponent implements OnInit {
+
     private readonly PAGE_SIZE = 10;
 
     public queryResult: any = {};
@@ -28,14 +37,32 @@ export class ProductComponent implements OnInit {
         { title: 'Reorder Level', key: 'reorderlevel', isSortable: true },
         { title: 'Discontinued', key: 'discontinued', isSortable: true },
         { title: '', key: '', isSortable: false }
-    ]
+    ];
+
+    public suppliers: any[];
+    public categories: any[];
 
     constructor(private notificationService: NotificationService,
         private productService: ProductService,
-        private loadingBarService: SlimLoadingBarService) { }
+        private categoryService: CategoryService,
+        private supplierService: SupplierService,
+        private loadingBarService: SlimLoadingBarService,
+        private modalService: NgbModal) { }
 
     ngOnInit() {
         this.populateProducts();
+
+        var sources = [
+            this.supplierService.getSuppliers(),
+            this.categoryService.getCategories(),
+        ];
+
+        Observable.forkJoin(sources).subscribe(data => {
+            this.suppliers = data[0];
+            this.categories = data[1];
+        }, error => {
+            this.notificationService.error(error);
+        });
     }
 
     private populateProducts() {
@@ -70,5 +97,44 @@ export class ProductComponent implements OnInit {
     onFilterChange() {
         this.query.page = 1;
         this.populateProducts();
+    }
+
+    // details    
+    public productDetails: any;
+
+    openProductDetails(content: any, productId: number) {
+        this.loadingBarService.start();
+
+        this.productService.getProduct(productId)
+            .subscribe(result => {
+                this.productDetails = result;
+                this.loadingBarService.complete();
+                this.notificationService.info('Product details loaded');
+
+                this.modalService.open(content);
+            },
+            error => {
+                this.notificationService.error(error);
+            });
+    }
+
+    // update
+    public productUpdate: any;
+
+    openProductUpdate(content: any, productId: number) {
+        this.loadingBarService.start();
+
+        this.productService.getProduct(productId)
+            .subscribe(result => {
+                this.productUpdate = result;
+                this.loadingBarService.complete();
+                this.notificationService.info('Product update loaded');
+
+                this.modalService.open(content);
+            },
+            error => {
+                this.loadingBarService.complete();
+                this.notificationService.error(error);
+            });
     }
 }
